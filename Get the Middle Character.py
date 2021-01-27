21d23c5d7d1396990050cd3437bc6f81663a5794
Nikolay <yarik.mik@gmail.com>
2021-01-28 05:04:01 +0700

    get middle task

---
+++ Get the Middle Character.py
@@ -0,0 +1,26 @@
+"""
+You are going to be given a word. Your job is to return the middle character of the word.
+If the word's length is odd, return the middle character.
+If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+"""
+
+
+# def get_middle(s):
+#     return s[int(len(s) / 2-1):int(len(s) / 2+1)] if len(s) % 2 == 0 else s[len(s) // 2]
+
+def get_middle(s):
+    i = (len(s) - 1) // 2
+    return s[i:-i] or s
+
+print(get_middle('asdFGqwr'))
